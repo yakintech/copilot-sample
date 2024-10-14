f9b8bef66772fc4111b21d239badd6060ea92169
cagatay.yildiz <cagatay@pixupplay.com>
2024-10-14 11:47:16 +0300

    Refactor app.js for improved readability and add .env configuration file

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require('helmet'); // Helmet paketini ekleyin
-const rateLimit = require('express-rate-limit'); // express-rate-limit paketini ekleyin
+const helmet = require('helmet'); 
+const rateLimit = require('express-rate-limit'); 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Helmet kullanımı
-app.use(helmet()); // Helmet'i kullanıma alın
+app.use(helmet());
 
-// Rate limiting
+
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 dakika
-  max: 100 // 15 dakika içinde 100 istek
+  windowMs: 15 * 60 * 1000, 
+  max: 100
 });
-app.use(limiter); // Rate limiting'i kullanıma alın
+app.use(limiter); 
 
-// MongoDB bağlantısı
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB bağlantısı başarılı');
@@ -25,14 +24,14 @@ mongoose.connect(process.env.MONGODB_URI)
     console.error('MongoDB bağlantı hatası:', err);
   });
 
-// Middleware
+
 app.use(express.json());
 
-// Rotalar
+
 app.use('/api', require('./routes/productRoutes'));
 app.use('/api', require('./routes/categoryRoutes'));
 
-// Sunucuyu başlatma
+
 app.listen(port, () => {
   console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
 });
